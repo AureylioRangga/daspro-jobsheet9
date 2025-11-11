41a60fd393f620587baf794957ab905b918791e4
Aureylio <aureyliorangga2@gmail.com>
2025-11-11 09:08:20 +0700

    Percobaan 2

--- ArrayNilai07.java
+++ ArrayNilai07.java
@@ -3,7 +3,7 @@ public class ArrayNilai07 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
 
-        int[] nilaiAkhir = new int[10];
+        int[] nilaiAkhir = new int[10]; 
 
         for (int i = 0; i < nilaiAkhir.length; i++) {
             System.out.print("Masukkan nilai akhir ke-"+i+" : ");
