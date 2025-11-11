a34320bdae13a53d0f8258259d0855e0c31b671e
Aureylio <aureyliorangga2@gmail.com>
2025-11-11 09:07:44 +0700

    Percobaan 1

--- ArrayBilangan07.java
+++ ArrayBilangan07.java
@@ -7,7 +7,7 @@ public class ArrayBilangan07 {
         bil[0] = 5;
         bil[1] = 13;
         bil[2] = -7;
-        bil[3] = 17;
+        bil[3] = 17; 
 
        for (int i = 0; i < 4; i++) {
               System.out.println(bil[i]);
