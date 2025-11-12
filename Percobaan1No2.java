b89e36667af7fb1e3ee40028e8c24ba658d3eb86
Aureylio <aureyliorangga2@gmail.com>
2025-11-12 09:37:22 +0700

    Percobaan1No2

---
+++ Percobaan1No2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Percobaan1No2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int bil[] = {5, 13, -7, 17};
+
+       for (int i = 0; i < 4; i++) {
+              System.out.print(bil[i]+",");
+       }
+    }
+}
